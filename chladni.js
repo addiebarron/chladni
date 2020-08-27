e930dfb447b0ee396413a21fc7499bc61fbd93bb
addiebarron <addiebarronemail@gmail.com>
2020-08-27 15:56:29 -0400

    Refactored for a11y; code cleanup

--- chladni.js
+++ chladni.js
@@ -1,71 +1,53 @@
+let particles, sliders, m, n, v, N;
+
 // chladni frequency params
 let a=1, b=1;
-
 // vibration strength params
 let A = 0.02;
 let minWalk = 0.002;
 
-// settings
-const nParticles = 10000;
-const canvasSize = 600;
+const settings = {
+  nParticles : 10000,
+  canvasSize : [600, 600],
+  drawHeatmap : false
+}
 
-// pi
 const pi = 3.1415926535;
 
-// draw the underlying chladni function
-let drawFunction = false;
+// chladni 2D closed-form solution - returns between -1 and 1
+const chladni = (x, y, a, b, m, n) => 
+  a * sin(pi*n*x) * sin(pi*m*y) 
++ b * sin(pi*m*x) * sin(pi*n*y);
 
-function setup() {
-  createCanvas(canvasSize, canvasSize);
+
+/* Initialization */
+
+const DOMinit = () => {
+  let canvas = createCanvas(...settings.canvasSize);
+  canvas.parent('sketch-container');
 
   // sliders
   sliders = {
     m : select('#mSlider'), // freq param 1
     n : select('#nSlider'), // freq param 2
     v : select('#vSlider'), // velocity
-    N : select('#Nslider'), // number
+    num : select('#numSlider'), // number
   }
+}
 
+const setupParticles = () => {
   // particle array
   particles = [];
-  for (let i = 0; i < nParticles; i++) {
+  for (let i = 0; i < settings.nParticles; i++) {
     particles[i] = new Particle();
   }
 }
 
-function draw() {
-  background(30);
-  stroke(255);
-  
-  // update parameters via sliders
-  m = sliders.m.value();
-  n = sliders.n.value();
-  v = sliders.v.value();
-  N = sliders.N.value();
-    
-  // draw the function in the background
-  if (drawFunction) {
-  	let res = 3;
-    for (let i = 0; i <= width; i+=res) {
-      for (let j = 0; j <= height; j+=res) {
-        let eq = chladni(i/width, j/height, a, b, m, n);
-        noStroke();
-        fill((eq + 1) * 127.5);
-        square(i, j, res);
-      }
-    }
-  }
 
-  movingParticles = particles.slice(0, N);
-
-  // particle movement
-  for(let particle of movingParticles) {
-    particle.move();
-    particle.show();
-  }
-}
+/* Particle dynamics */
 
 class Particle {
+
   constructor() {
     this.x = random(0,1);
     this.y = random(0,1);
@@ -93,13 +75,13 @@ class Particle {
   }
 
   updateOffsets() {
-  	// handle edges
-  	if (this.x <= 0) this.x = 0;
-  	if (this.x >= 1) this.x = 1;
-  	if (this.y <= 0) this.y = 0;
-  	if (this.y >= 1) this.y = 1;
+    // handle edges
+    if (this.x <= 0) this.x = 0;
+    if (this.x >= 1) this.x = 1;
+    if (this.y <= 0) this.y = 0;
+    if (this.y >= 1) this.y = 1;
 
-  	// convert to screen space
+    // convert to screen space
     this.xOff = width * this.x; // (this.x + 1) / 2 * width;
     this.yOff = height * this.y; // (this.y + 1) / 2 * height;
   }
@@ -110,7 +92,55 @@ class Particle {
   }
 }
 
-// returns between -1 and 1
-function chladni(x, y, a, b, m, n) {
-  return a * sin(pi*n*x) * sin(pi*m*y) + b * sin(pi*m*x) * sin(pi*n*y);
+const moveParticles = () => {
+  let movingParticles = particles.slice(0, N);
+
+  // particle movement
+  for(let particle of movingParticles) {
+    particle.move();
+    particle.show();
+  }
+}
+
+const updateParams = () => {
+  m = sliders.m.value();
+  n = sliders.n.value();
+  v = sliders.v.value();
+  N = sliders.num.value();
+}
+
+const drawHeatmap = () => {
+  // draw the function heatmap in the background (not working)
+  if (settings.drawHeatmap) {
+    let res = 3;
+    for (let i = 0; i <= width; i+=res) {
+      for (let j = 0; j <= height; j+=res) {
+        let eq = chladni(i/width, j/height, a, b, m, n);
+        noStroke();
+        fill((eq + 1) * 127.5);
+        square(i, j, res);
+      }
+    }
+  }
+}
+
+const wipeScreen = () => {
+  background(30);
+  stroke(255);
+}
+
+
+/* Timing */
+
+// run at DOM load
+function setup() {
+  DOMinit();
+  setupParticles();
+}
+// run each frame
+function draw() {
+  wipeScreen();
+  updateParams();
+  drawHeatmap();
+  moveParticles();
 }
